937c166a56e138ac4c3d4f70259c10450a4f96d9
dimi1 <dimi1010100@gmail.com>
2019-09-09 15:33:54 +0300

    Initial

---
+++ Resources/Database/Model/DiscordRole.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DiscordVerifyBot.Resources.Database.Model
+{
+    public class DiscordRole
+    {
+        public enum ActionType
+        {
+            None,
+            Add,
+            Remove
+        }
+
+        [Key] public ulong RoleId { get; set; }
+
+        [Required] public ulong GuildId { get; set; }
+
+        [Required] public ActionType Action { get; set; }
+    }
+}
