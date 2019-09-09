7f3fad51dea819ec40fa03660a1ae2fdc95c9a94
dimi1 <dimi1010100@gmail.com>
2019-09-09 21:54:53 +0300

    Initial

---
+++ Core/Commands/General/VerifyModule.Roles.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Discord;
+using Discord.Commands;
+
+using DiscordVerifyBot.Core.Services;
+using DiscordVerifyBot.Core.Handlers;
+using DiscordVerifyBot.Resources.Database.Model;
+
+namespace DiscordVerifyBot.Core.Commands.General
+{
+    public partial class VerifyModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("Add-Role")]
+        [RequireContext(ContextType.Guild)]
+        [RequireOwner(Group = "Invoke Access")]
+        [RequireUserPermission(GuildPermission.ManageRoles, Group = "Invoke Access")]
+        public async Task AddRole(string condition, IRole role)
+        {
+            condition = condition.ToLower();
+
+            DiscordRole.ActionType action;
+            switch (condition)
+            {
+                case "add":
+                    action = DiscordRole.ActionType.Add;
+                    break;
+                case "remove":
+                    action = DiscordRole.ActionType.Remove;
+                    break;
+                default:
+                    await _replyservice.ReplyEmbedAsync(context: Context,
+                        message: "Command did not specify action type.",
+                        description: "Please use the syntax: 'Add-Role [Add or Remove] <RoleMention>'");
+                    return;
+            }
+
+            await DiscordRoleDataHandler.AddGuildRole(
+                roleId: role.Id,
+                guildId: Context.Guild.Id,
+                action: action
+                );
+
+            _logger.Log(Message: $"User {Context.User.Id} in {Context.Guild.Id} added role {role.Id} with action { action.ToString() }", Source:"Commands");
+
+            await _replyservice.ReplyEmbedAsync(context: Context,
+                message: $"Role {role.Mention} has been added with the action {action.ToString()}.");
+        }
+
+        [Command("Remove-Role")]
+        [RequireContext(ContextType.Guild)]
+        [RequireOwner(Group = "Invoke Access")]
+        [RequireUserPermission(GuildPermission.ManageRoles, Group = "Invoke Access")]
+        public async Task RemoveRole(IRole role)
+        {
+            await DiscordRoleDataHandler.RemoveGuildRole(role.Id, Context.Guild.Id);
+
+            _logger.Log(Message: $"User {Context.User.Id} in {Context.Guild.Id} removed role {role.Id} from the database", Source: "Commands");
+
+            await _replyservice.ReplyEmbedAsync(context: Context,
+                message: $"Role {role.Mention} has been removed from the database.");
+        }
+    }
+}
