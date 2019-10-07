7ce2db6cf2268505b01927c670135761d25eca6f
Dimitar Krastev <dimi1010100@gmail.com>
2019-10-07 22:41:56 +0300

    Added UtilitiesModule.

---
+++ Core/Commands/General/UtilitiesModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+using DiscordVerifyBot.Core.Services;
+
+namespace DiscordVerifyBot.Core.Commands.General
+{
+    class UtilitiesModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly IReplyService _replyservice;
+        private readonly ILoggerService _logger;
+
+        public UtilitiesModule(IReplyService replyService, ILoggerService logger)
+        {
+            _replyservice = replyService ?? throw new ArgumentNullException(paramName: "replyService");
+            _logger = logger ?? throw new ArgumentNullException(paramName: "logger");
+        }
+
+        #region Utility
+
+        [Command("Ping"), Summary("Responds with bot latency")]
+        public async Task PingAsync()
+        {
+            await _replyservice.ReplyEmbedAsync(context: Context, message: "Pong", description: "Latency to Gateway: " + Context.Client.Latency);
+        }
+
+        #endregion
+    }
+}
