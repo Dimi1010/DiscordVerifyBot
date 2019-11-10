7439606eeef521c26f43bfd51dcf0d8fcea7f429
Dimitar Krastev <dimi1010100@gmail.com>
2019-11-10 18:44:43 +0200

    Added commands to show user permissions.

--- Core/Commands/General/VerifyModule.Audit.cs
+++ Core/Commands/General/VerifyModule.Audit.cs
@@ -12,8 +12,10 @@ namespace DiscordVerifyBot.Core.Commands.General
 {
     partial class VerifyModule : ModuleBase<SocketCommandContext>
     {
-        [Command("List-Last"), Alias("l", "Show-Last")]
-        public async Task ListLastForms(int records = 5)
+        #region Verification Forms
+
+        [Command("Show-Last"), Alias("sl")]
+        public async Task ShowLastForms(int records = 5)
         {
             if(records <= 0)
             {
@@ -47,5 +49,103 @@ namespace DiscordVerifyBot.Core.Commands.General
 
             await _replyservice.ReplyEmbedAsync(Context, embed);
         }
+
+        #endregion
+
+        #region User Permissions
+
+        [Command("Show-Permissions")]
+        public async Task ShowUserPermissions()
+        {
+            var users = DiscordUserDataHandler.GetGuildUsers(Context.Guild.Id);
+
+            if (users.Count < 1)
+            {
+                await _replyservice.ReplyEmbedAsync(context: Context, message: "No users have special permissions for this bot.");
+                return;
+            }
+
+            var embed = new EmbedBuilder
+            {
+                Author = new EmbedAuthorBuilder
+                {
+                    Name = $"Showing user permissions.",
+                    IconUrl = Context.Client.CurrentUser.GetAvatarUrl()
+                },
+                Color = Color.Blue,
+                Timestamp = DateTimeOffset.UtcNow
+            };
+
+            foreach(var user in users)
+            {
+                string permLevelString;
+                switch (user.PermissionLevel)
+                {
+                    case Resources.Database.Model.DiscordGuildUser.PermissionLevels.Verify:
+                        permLevelString = "Verifier";
+                        break;
+                    case Resources.Database.Model.DiscordGuildUser.PermissionLevels.Approve:
+                        permLevelString = "Approver";
+                        break;
+                    default:
+                        permLevelString = "None";
+                        break;
+                }
+
+                var field = new EmbedFieldBuilder
+                {
+                    Name = Context.Guild.GetUser(user.UserId).Username,
+                    Value = "Permission: " + permLevelString
+                };
+
+                embed.AddField(field);
+            }
+
+            await _replyservice.ReplyEmbedAsync(Context, embed);
+        }
+
+        [Command("Show-Permissions")]
+        public async Task ShowUserPermissions(IGuildUser user)
+        {
+            var dbUser = DiscordUserDataHandler.GetGuildUserById(user.Id, Context.Guild.Id);
+
+            var embed = new EmbedBuilder
+            {
+                Author = new EmbedAuthorBuilder
+                {
+                    Name = $"Showing user permissions.",
+                    IconUrl = Context.Client.CurrentUser.GetAvatarUrl()
+                },
+                Color = Color.Blue,
+                Timestamp = DateTimeOffset.UtcNow
+            };
+
+            string permLevelString;
+            switch (dbUser.PermissionLevel)
+            {
+                case Resources.Database.Model.DiscordGuildUser.PermissionLevels.Verify:
+                    permLevelString = "Verifier";
+                    break;
+                case Resources.Database.Model.DiscordGuildUser.PermissionLevels.Approve:
+                    permLevelString = "Approver";
+                    break;
+                default:
+                    permLevelString = "None";
+                    break;
+            }
+
+            var field = new EmbedFieldBuilder
+            {
+                Name = user.Username,
+                Value = "Permission: " + permLevelString
+            };
+
+            embed.AddField(field);
+            
+            await _replyservice.ReplyEmbedAsync(Context, embed);
+        }
+
+        #endregion
+
     }
 }
