d1a07eeaabc81a8d69718c8cb773279c11e2f849
Dimitar Krastev <dimi1010100@gmail.com>
2019-11-10 18:01:24 +0200

    Replaced logging with Serilog Logging framework.

---
+++ Core/Logging/Enrichers/ThreadIdEnricher.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+
+using Serilog.Core;
+using Serilog.Events;
+
+namespace DiscordVerifyBot.Core.Logging
+{
+    class ThreadIdEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+                "ThreadId", Thread.CurrentThread.ManagedThreadId));
+        }
+    }
+}
